cae90efe857cdb866fd98bae47127593a569a3f8
Verum <sergeyzavalin@outlook.com>
2025-02-26 09:19:26 +0700

    1.2 теперь все сообщения связаны с БД

--- BotCode/routers/common/messages.py
+++ BotCode/routers/common/messages.py
@@ -3,13 +3,14 @@
 
 from aiogram import Router, types
 from BotLibrary import *
+from SQLite3.bd import base_sql
 
 # Настройка экспорта модулей и роутера
 __all__ = ("router",)
 router = Router(name="common_msg_router")
 
 
-# Хэндлер на все сообщения и записывает данные
 @router.message()
 async def handle_all_messages(message: types.Message):
-    await logger_msg(message)
+    await base_sql(message)
+    await logger_msg(message)  # Это твой метод для логирования
