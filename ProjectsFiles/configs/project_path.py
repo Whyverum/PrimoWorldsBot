5d0737cd1ec8dfadc39390c6e20501981848b1a1
Verum <sergeyzavalin@outlook.com>
2025-02-26 11:19:35 +0700

    1.3 Исправлены небольшие ошибки

--- ProjectsFiles/configs/project_path.py
+++ ProjectsFiles/configs/project_path.py
@@ -9,24 +9,24 @@ class TypeDirectory:
     Класс для хранения типов сообщений и директорий, которые нужно создать.
     """
     # Типы сообщений и список директорий для создания
-    private_msg : str = "Личные"
-    group_msg : str = "Группы"
+    private_msg: str = "Личные"
+    group_msg: str = "Группы"
 
     # Названия директорий-хранилищ
-    avatar : str = "Avatar"
-    photo : str = "Photo"
-    video : str = "Video"
-    videonote : str = "VideoNote"
-    gif : str = "GIF"
-    files : str = "Document"
-    voice : str = "Voice"
-    media_directories : List[str] = [avatar, photo, video, videonote, gif, files, voice]
+    avatar: str = "Avatar"
+    photo: str = "Photo"
+    video: str = "Video"
+    videonote: str = "VideoNote"
+    gif: str = "GIF"
+    files: str = "Document"
+    voice: str = "Voice"
+    media_directories: List[str] = [avatar, photo, video, videonote, gif, files, voice]
 
     # Названия директорий для аватарок
-    user_avatar : str = "Users"
-    chat_avatar : str = "Chats"
-    channel_avatar : str = "Channel"
-    avatar_directories : List[str] = [user_avatar, chat_avatar, channel_avatar]
+    user_avatar: str = "Users"
+    chat_avatar: str = "Chats"
+    channel_avatar: str = "Channel"
+    avatar_directories: List[str] = [user_avatar, chat_avatar, channel_avatar]
 
 
 # Класс создания директорий проекта
@@ -34,16 +34,16 @@ class ProjectPath:
     """
     Класс для хранения путей к проектам и логам.
     """
-    BotLogs : str = "BotLogs"
+    BotLogs: str = "BotLogs"
     bot_info_log_file: str = f"{BotLogs}/bot_info.logs"
     start_log_file: str = f"{BotLogs}/start.logs"
     debug_log_file: str = f"{BotLogs}/debug.log"
-    info_log_file : str = f"{BotLogs}/info.log"
+    info_log_file: str = f"{BotLogs}/info.log"
     warning_log_file: str = f"{BotLogs}/warning.log"
     error_log_file: str = f"{BotLogs}/error.log"
 
-    BotFiles : str = "BotFiles"
-    received_media : str = f"{BotFiles}/media"
-    received_avatars : str = f"{BotFiles}/avatars"
+    BotFiles: str = "BotFiles"
+    received_media: str = f"{BotFiles}/media"
+    received_avatars: str = f"{BotFiles}/avatars"
 
-    personal_media : str = "ProjectsFiles/media"
+    personal_media: str = "ProjectsFiles/media"
