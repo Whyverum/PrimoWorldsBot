cc56f11f8093fb491536188ec4b9e04a65753bf6
Verum <sergeyzavalin@outlook.com>
2025-03-02 22:42:55 +0700

    1.8 Добавлена возможность вставлять ссылки в имя пользователя

--- BotLibrary/validators/username.py
+++ BotLibrary/validators/username.py
@@ -4,7 +4,7 @@
 from aiogram.types import Message
 
 # Настройка экспорта из модуля
-__all__ = ("username",)
+__all__ = ("username", "username_to_text")
 
 # Функция получения юзера или ID пользователя
 def username(message: Message) -> str:
@@ -17,3 +17,8 @@ def username(message: Message) -> str:
     if message.from_user:
         return f"@{message.from_user.username}" if message.from_user.username else f"@{message.from_user.id}"
     return "@Unknown_User"  # Если from_user отсутствует
+
+
+# Функция получение имени пользователя + ссылка на него
+def username_to_text(message: Message) -> str:
+    return f'<b><a href="tg://user?id={message.from_user.id}">{message.from_user.full_name}</a></b>'
