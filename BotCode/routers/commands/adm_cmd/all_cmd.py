2c0e45158146c701bd5f5f8de1ab40966f5ddbaa
Verum <sergeyzavalin@outlook.com>
2025-03-06 05:12:45 +0700

    1.10 В Подключение новых возможностей

--- BotCode/routers/commands/adm_cmd/all_cmd.py
+++ BotCode/routers/commands/adm_cmd/all_cmd.py
@@ -1,16 +1,21 @@
 # BotCode/routers/commands/adm_cmd/all_cmd.py
-# Работа с командой /all, для всеобщего призыва
+#
 
+from BotCode.utils import hidden_admins_message
 from BotLibrary import CommandHandler
 
 # Настройки экспорта в модули
 __all__ = ("all_cmd",)
 
-# Создание команды /start с несколькими медиа
+# Текст, который вы хотите передать по умолчанию
+custom_text = "<b>Важное уведомление!</b>"
+
+# Создание команды /all с несколькими медиа
 all_cmd = CommandHandler(
     name="all",
     description="Всеобщий призыв",
     keywords=["all", "фдд", "@all"],
     callbackdata=["keywords"],
-    text_msg="ТЕСТ! НЕ РАБОТАЕТ!",
+    media="command",
+    func=[lambda message, *args: hidden_admins_message(message, msg=False, text=message.text.split(" ", 1)[1] if len(message.text.split(" ", 1)) > 1 else custom_text)],
 )
--- BotCode/utils/__init__.py
+++ BotCode/utils/__init__.py
@@ -2,10 +2,12 @@
 # Инициализация пакета utils, для работы с механиками
 
 from aiogram import Router
+from .admin_list import admin_lists
+from .all_admins_hide import hidden_admins_message
 from .weather_api import get_weather
 
 # Объявление роутера и настройка экспорта модулей
-__all__ = ("router", "get_weather")
+__all__ = ("router", "get_weather", "admin_lists", "hidden_admins_message")
 router = Router(name="utils_head_router")
 
 # Идет самым последним, если другие роутеры не сработали
