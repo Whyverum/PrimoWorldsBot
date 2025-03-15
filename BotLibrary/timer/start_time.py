f0a54282c7d28336017d2ca5dd6a5604e4a5e9e1
Verum <sergeyzavalin@outlook.com>
2025-03-16 02:11:30 +0700

    2.1 Исправление документации

--- BotLibrary/timer/start_time.py
+++ BotLibrary/timer/start_time.py
@@ -15,25 +15,28 @@ scheduler = AsyncIOScheduler(timezone=get_localzone().key)
 
 
 # Функция получение иного времени
-def get_city_time(city: str = 'Europe/Moscow') -> str:
+def get_city_time(city: str = 'Europe/Moscow',
+                  time_format: str = BotVar.time_format) -> str:
     """
     Получение текущего времени по иному городскому времени.
 
-    :param city: Город, что будет вторым временем
+    :param city: Город, что будет вторым временем.
+    :param time_format: Шаблон форматирования времени (конфиг).
     :return: Строка, представляющая время в формате, заданном в BotVar.time_format.
     """
     # Устанавливаем временную зону для Москвы
     city_tz = pytz.timezone(city)
     # Возвращаем строку с форматом времени
-    return datetime.now(city_tz).strftime(BotVar.time_format)
+    return datetime.now(city_tz).strftime(time_format)
 
 
 # Функция получение времени хоста
-def get_host_time() -> str:
+def get_host_time(time_format: str = BotVar.time_format) -> str:
     """
     Получение текущего времени хоста (локального времени).
 
-    :return: Строка, представляющая локальное время в формате, заданном в BotVar.time_format.
+    :param time_format: Шаблон форматирования времени (конфиг).
+    :return: Строка, представляющая локальное время в формате format.
     """
     # Возвращаем строку с форматом времени
-    return datetime.now().strftime(BotVar.time_format)
+    return datetime.now().strftime(time_format)
