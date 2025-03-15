b8f7ce5b2a646d8a376bd1cc97d410189e1ab2c0
Verum <sergeyzavalin@outlook.com>
2025-03-16 03:18:22 +0700

    2.2 Мелкие улучшения и аннотации файлы

--- BotCode/__init__.py
+++ BotCode/__init__.py
@@ -4,12 +4,14 @@
 from aiogram import Router
 from .routers import router as all_routers
 from .inline import router as inline_routers
+from .easteggs import router as easteggs_router
 
 # Объявление главного роутера
 router = Router(name="main_router")
 
 # Список подключаемых роутеров сверху-вниз
 router.include_routers(
-    all_routers,
-            inline_routers,
+    inline_routers,
+    easteggs_router,
 )
+router.include_router(all_routers)
